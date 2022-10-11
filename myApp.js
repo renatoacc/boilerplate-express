fbf7db7e74bb65df72e16376fcc49ad7bbc4de70
Renato Costa <renatoaccosta@gmail.com>
2022-10-11 17:06:30 +0100

    clean code 3

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ const absolutePath = __dirname + "/views/index.html";
 require('dotenv').config();
 
 app.use(express.static(__dirname + "/public"));
-
+app.use("/public", express.static(__dirname + "/public"));
 //middleware call
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
