6f6aadd5be7e0700f08594dcbfbc8aac0d06089c
Renato Costa <renatoaccosta@gmail.com>
2022-10-11 16:24:48 +0100

    update root

--- myApp.js
+++ myApp.js
@@ -28,7 +28,7 @@ app.get("/json", (req, res, next) => {
     res.json(objectJSON);
 });
 
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 })
