fc3d0a697db4262c838332a79dde954efe899f3f
Renato Costa <renatoaccosta@gmail.com>
2022-10-11 12:40:05 +0100

    console hello world

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
 
 
@@ -36,4 +37,4 @@ let app = express();
 
 
 
- module.exports = app;
+module.exports = app;
