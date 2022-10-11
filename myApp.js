86791c8c85c7e294ba405c05b0bb3b6d257eb7a8
Renato Costa <renatoaccosta@gmail.com>
2022-10-11 12:47:50 +0100

    app get hello express

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
 
+// app.listen(()=> 3000);
 console.log("Hello World");
 
-
+app.get('/', (req, res, next) => {
+    res.send("Hello Express");
+})
 
 
 
